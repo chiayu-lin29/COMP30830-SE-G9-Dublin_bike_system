8ec604e420e1051659d62383b9f1616eb8f77683
chiayu-lin29 <71386548+chiayu2999@users.noreply.github.com>
2025-04-12 10:37:17 +0100

    save local to merge

--- db/db_connection.py
+++ db/db_connection.py
@@ -20,14 +20,28 @@
 # MYSQL_DB = "dbbikes"
 
 
+# import mysql.connector
+# def get_db_connection():
+#     """Returns a MySQL connection object and cursor."""
+#     conn = mysql.connector.connect(
+#         host="16.16.13.59",
+#         user="root",
+#         password="2025!Group9",
+#         database="dbbikes"
+#     )
+#     cursor = conn.cursor()
+#     return conn, cursor
+
+
 import mysql.connector
+
 def get_db_connection():
     """Returns a MySQL connection object and cursor."""
     conn = mysql.connector.connect(
-        host="16.16.13.59",
+        host="localhost",  # e.g., "localhost"
         user="root",
-        password="2025!Group9",
-        database="dbbikes"
+        password="password",
+        database="SWE"
     )
     cursor = conn.cursor()
     return conn, cursor
