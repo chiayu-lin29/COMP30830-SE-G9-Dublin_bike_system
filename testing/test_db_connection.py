52e7eec105a8edb7d3db4c0cd3fc00aa259dd56f
chiayu-lin29 <71386548+chiayu2999@users.noreply.github.com>
2025-04-12 11:52:51 +0100

    revise testing

--- testing/test_db_connection.py
+++ testing/test_db_connection.py
@@ -1,7 +1,7 @@
 # Test for a successful connection to the database
 # test_db_connection.py
 import unittest
-from SQLpw import USER, PASSWORD, PORT, DB, URI
+from db.SQLpw import USER, PASSWORD, PORT, DB, URI
 import mysql.connector
 
 class TestDBConnection(unittest.TestCase):
